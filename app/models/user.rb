aa2a24d44f3848c4feb0394fc995c4a41fa0ffcf
joerails <joerails2012@gmail.com>
2013-07-08 20:46:12 +0300

    small adjustment on the user  model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
     validates :password_confirmation, presence: true
+    after_validation { self.errors.messages.delete(:password_digest) }
 end
